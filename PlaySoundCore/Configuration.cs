985b5567f15b07f64fb4b805111c71c9f4a3c429
Mark Olbert <mark@arcabama.com>
2021-11-10 08:57:19 -0800

    Initial commit

---
+++ PlaySoundCore/Configuration.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PlaySoundCore
+{
+    public class Configuration
+    {
+        private string[] _audioExt;
+        private DirectoryInfo _dirInfo;
+
+        public string[] Extensions
+        {
+            get
+            {
+                if( _audioExt == null || _audioExt.Length == 0 )
+                    _audioExt = new[] { ".wav", ".mp3" };
+
+                return _audioExt;
+            }
+
+            set => _audioExt = value;
+        }
+
+        public FileInfo SoundFile { get; set; }
+
+        public DirectoryInfo SoundDirectory
+        {
+            get
+            {
+                if( _dirInfo == null )
+                {
+                    if( SoundFile != null )
+                        _dirInfo = SoundFile.Directory;
+                }
+
+                return _dirInfo;
+            }
+
+            set => _dirInfo = value;
+        }
+
+        public bool GetSoundFile( out string fileName )
+        {
+            fileName = null;
+
+            if( SoundFile != null && File.Exists( SoundFile.FullName ) )
+            {
+                fileName = SoundFile.FullName;
+                return true;
+            }
+
+            if( SoundDirectory == null || !Directory.Exists(SoundDirectory.FullName) )
+                return false;
+
+            var files = Directory.GetFiles(
+                    SoundDirectory.FullName,
+                    "*.*",
+                    searchOption : SearchOption.AllDirectories )
+                .Where( f =>
+                    Extensions.Any( x => x.Equals( Path.GetExtension( f ), StringComparison.OrdinalIgnoreCase ) ) )
+                .ToList();
+
+            if( files.Count == 0 )
+                return false;
+
+            var random = new Random();
+
+            fileName = files[ random.Next( files.Count ) ];
+
+            return true;
+        }
+    }
+}
---
+++ PlaySoundCore/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Media;
+using System.Reflection.Metadata.Ecma335;
+
+namespace PlaySoundCore
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var command = new RootCommand()
+            {
+                new Option<FileInfo>(
+                    new[] { "-f", "--file" },
+                    description : "Sound file to play" )
+                {
+                    Name = "SoundFile"
+                },
+
+                new Option<DirectoryInfo>(
+                    new[] { "-d", "--directory" },
+                    getDefaultValue:()=>new DirectoryInfo("c:/Sounds"),
+                    description : "Directory of sound files to choose" )
+                {
+                    Name = "SoundDirectory"
+                },
+
+                new Option<string[]>(
+                    new[] { "-x", "--extensions" },
+                    getDefaultValue: ()=>new []{".wav", ".mp3"},
+                    description : "Valid audio file extensions" )
+                {
+                    Name = "Extensions"
+                }
+            };
+
+            Configuration config = new Configuration();
+
+            command.Handler = CommandHandler.Create( (Configuration c) =>
+            {
+                config.Extensions = c.Extensions;
+                config.SoundFile = c.SoundFile;
+                config.SoundDirectory = c.SoundDirectory;
+            });
+
+            var result = command.Invoke( args );
+
+            if( !config.GetSoundFile(out var fileName))
+            {
+                Environment.ExitCode = 1;
+                return;
+            }
+
+            var player = new SoundPlayer( fileName );
+            player.PlaySync();
+        }
+    }
+}
